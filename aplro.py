f141e50b25c3c050b88c1887e68bfe835a8c5162
darkoboy <118077486+dewaahr@users.noreply.github.com>
2023-05-09 12:38:46 +0700

    Update aplro.py

--- aplro.py
+++ aplro.py
@@ -12,22 +12,39 @@
 
 
 
-data_nota={"toko":"Toko Buku Aku",
-           "pembeli":"Dewa",
-           "nama barang":["Pensil 2B","Buku",'Penghapus','Penggaris'],
-           'harga': [3500,2500,1000,1000]
-}
 
-belanja=[2,1,3,2]
+# data_nota={"toko":"Toko Buku Aku",
+#            "pembeli":"Dewa",
+#            "nama barang":["Pensil 2B","Buku",'Penghapus','Penggaris'],
+#            'harga': [3500,2500,1000,1000]
+# }
 
-print(f'Nama TOKO:{data_nota["toko"]}')
-print(f'Nama Pembeli:{data_nota["pembeli"]}')
-totalharga=0
-i=0
-for barang in data_nota['nama barang']:
-    print(barang,':',belanja[i],"Buah  |  Total Harga:",(data_nota["harga"][i])*belanja[i])
-    totalharga=totalharga+(data_nota["harga"][i])*belanja[i]
-    i=i+1
-print(totalharga)
-print('jumlah barang:',sum(belanja))
-print('Harga setelah diskon:',totalharga-totalharga*0.2)
+# belanja=[2,1,3,2]
+
+# print(f'Nama TOKO:{data_nota["toko"]}')
+# print(f'Nama Pembeli:{data_nota["pembeli"]}')
+# totalharga=0
+# i=0
+# for barang in data_nota['nama barang']:
+#     print(barang,':',belanja[i],"Buah  |  Total Harga:",(data_nota["harga"][i])*belanja[i])
+#     totalharga=totalharga+(data_nota["harga"][i])*belanja[i]
+#     i=i+1
+# print(totalharga)
+# print('jumlah barang:',sum(belanja))
+# print('Harga setelah diskon:',totalharga-totalharga*0.2)
+
+
+
+
+angka={"genap":[],"ganjil":[]}
+
+while True:
+    angka_input=int(input('Masukan angka (negatif untuk berhenti): '))
+    if angka_input<0:
+        print(angka)
+        break
+    if angka_input%2==0:
+        angka["genap"].append(angka_input)
+    else: angka["ganjil"].append(angka_input)
+
+print(angka)
